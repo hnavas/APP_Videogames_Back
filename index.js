855baa869e089a443a8b61f337c4b172ed182e97
hnavas <hendri.navas@gmail.com>
2023-02-03 00:49:11 -0300

    Cambios para el deploy

--- index.js
+++ index.js
@@ -17,14 +17,16 @@
 //     =====`-.____`.___ \_____/___.-`___.-'=====
 //                       `=---='
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+require('dotenv').config();
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
-const fn = require('../api/src/middlewares/funciones.js');
+const port = process.env.PORT || 3001; 
+// const fn = require('../api/src/middlewares/funciones.js');
 
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
   // fn.getAllGenres();
-  server.listen(3001, () => {
-    console.log('%s listening at 3001'); // eslint-disable-line no-console
+  server.listen(port, () => {
+    console.log(`Server raised in port ${port}`); // eslint-disable-line no-console
   });
 });
