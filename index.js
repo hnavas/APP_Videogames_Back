b2dfe2c57fa3ccbc7197cc349cde1b9ef5c11e90
hnavas <hendri.navas@gmail.com>
2023-02-03 02:06:03 -0300

    update-1

--- index.js
+++ index.js
@@ -21,11 +21,11 @@ require('dotenv').config();
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 const port = process.env.PORT || 3001; 
-const fn = require('../api/src/middlewares/funciones.js');
+// const fn = require('../api/src/middlewares/funciones.js');
 
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
-  fn.getAllGenres();
+  // fn.getAllGenres();
   server.listen(port, () => {
     console.log(`Server raised in port ${port}`); // eslint-disable-line no-console
   });
